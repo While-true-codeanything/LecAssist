83fae9f9edb126b38f17c0cacf6066433a0f88c1
Whiskas <pavlov.erg@gmail.com>
2023-12-25 11:21:24 +0300

    Web final materials

--- web/web_sub/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('login/', views.open_login),
-    path('register/', views.open_register),
-    path('upload/', views.open_upload_page)
-]
